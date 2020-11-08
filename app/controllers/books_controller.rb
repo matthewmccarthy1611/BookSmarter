3b6ee32a024f1e4ac08d832eac52b15a4403fb5e
mmccarthy64 <mmccarthy64@hotmail.com>
2020-11-08 12:39:22 -0600

    Create books, comments controllers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,38 @@
 class BooksController < ApplicationController
+    def index
+        @books = Book.all
+      end
+    
+      def new
+      end
+    
+      def show
+        @book = Book.find_by(id: params[:id])
+      end
+    
+      def edit
+      end
+    
+      def update
+      end
+    
+      def create
+        @book = Book.new(book_params)
+        if @book.save
+          redirect_to user_path(current_user), notice: "#{@book.title} was added to your library."
+        else
+          render 'new'
+        end
+      end
+    
+      def destroy
+        @book.destroy
+        redirect_to books_path, notice: "#{@book.title} was deleted."
+      end
+    
+      private
+    
+      def book_params
+        params.require(:book).permit(:title, :author, :description, :page_count, :user_id)
+      end
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,25 @@
 class CommentsController < ApplicationController
+    def index
+      end
+    
+      def new
+      end
+    
+      def show
+      end
+
+      def update
+      end
+    
+      def create
+      end
+    
+      def destroy
+      end
+    
+      private
+    
+      def comment_params
+        params.require(:comment).permit(:content, :book_id, :user_id)
+      end
 end
