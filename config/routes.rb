422638e3877848467e385135885351210bf94d9e
mmccarthy64 <mmccarthy64@hotmail.com>
2020-11-08 11:16:48 -0600

    Fix route typos

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root to: "pages#index"
-  devise_for :users, :controllers => {registrations: ‘registrations’ }
+  root to: "static#home"
+  devise_for :users, :controllers => {registrations: 'registrations' }
 end
