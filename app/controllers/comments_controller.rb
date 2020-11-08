9e42cbd82010d0ec07d21303d26ddd14c57f119e
mmccarthy64 <mmccarthy64@hotmail.com>
2020-11-08 15:18:01 -0600

    Add comments create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,25 +1,38 @@
 class CommentsController < ApplicationController
-    def index
-      end
-    
-      def new
-      end
-    
-      def show
-      end
+    before_action :authenticate_user!
 
-      def update
-      end
-    
-      def create
-      end
-    
-      def destroy
-      end
-    
-      private
-    
-      def comment_params
-        params.require(:comment).permit(:content, :book_id, :user_id)
-      end
+    def index
+    end
+
+    def new
+        @comment = Comment.new
+    end
+
+    def show
+    end
+
+    def update
+    end
+
+    def create
+        @comment = Comment.new(book_params)
+        @comment.user = current_user
+        if @book.save
+          redirect_to books_path, notice: "#{@book.title} was created."
+        else
+            @error 
+          render 'new'
+        end
+    end
+
+    def destroy
+        @comment.destroy
+        redirect_to books_path, notice: "#{@comment.title} was deleted."
+    end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:content, :book_id)
+    end
 end
