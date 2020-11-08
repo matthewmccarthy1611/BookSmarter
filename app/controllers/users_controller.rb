901ed0b5bb29bbe284415d8f1b023b3c7866f5de
mmccarthy64 <mmccarthy64@hotmail.com>
2020-11-08 15:11:20 -0600

    Update user routes, home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,11 @@
 class UsersController < ApplicationController
     
     def show
-        @user  = User.find_by_id(params[:id])
-        if @user
+        if @user  = User.find_by_id(params[:id])
             @user
         else
             flash[:alert] = "User does not exist"
-            redirect_to user_path(@user)
+            redirect_to root_path
         end
     end
 
