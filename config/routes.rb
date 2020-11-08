e9cd09aa3dbd1b973ad86f2592c1c25e9264802e
mmccarthy64 <mmccarthy64@hotmail.com>
2020-11-08 11:35:59 -0600

    Add devise scopes login, signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   root to: "static#home"
+
   devise_for :users, :controllers => {registrations: 'registrations' }
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
+  devise_scope :user do
+    get 'signup', to: 'devise/registrations#new'
+  end
+  
 end
